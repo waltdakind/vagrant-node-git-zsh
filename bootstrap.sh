5d2d54123b9783b7f06474ce655a6a802dc5944e
Walter Mattingly <waltdakind@gmail.com>
2016-11-04 20:01:54 -0400

    shorten wget output and remove red dpkg err

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,24 +2,26 @@
    echo '~~~ Apt-get update, upgrade and autoremove ~~~'
    echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
    echo -e ' \n '
-  sudo apt-get update 
-  sudo apt-get upgrade -y
-  sudo apt-get autoremove -y
+  sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile
+
+
+  sudo apt-get update 1> /dev/null 2>&1
+  sudo apt-get upgrade -y 1> /dev/null 2>&1
+  sudo apt-get autoremove -y 1> /dev/null 2>&1
 
    echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
    echo '~~~Git and Zsh ~~~'
 
-         if [ -n "$(ls -A /usr/bin/git)" ]
+         if [[ -a  /usr/bin/git ]]
       then
           echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
           echo "Git is already installed"
 
-          git config --global core.autocrlf true
 else
           echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-          echo "Attempting to Install Git..."
+          echo "Installing Git..."
 
-          sudo apt-get install -y git
+          sudo apt-get install -y git 1> /dev/null 2>&1
           # the line below helps avoid some errors caused 
           # by DOS style carriage returns
           # can be disabled in Mac/Linux hosts
@@ -27,34 +29,30 @@ else
 fi
    echo -e ' \n '
 
-         if [ -n "$(ls -A /usr/bin/zsh)" ]
+         if [[ -a /usr/bin/zsh ]]
       then
           echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
           echo "Zsh is already installed"
           echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 else
           echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-          echo "Attempting to Install Zsh..."
+          echo "Installing Zsh..."
 
-          sudo apt-get install -y zsh
+          sudo apt-get install -y zsh 1> /dev/null 2>&1
 fi
-   echo -e ' \n '
+
 
    echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
    echo '~~~ Installing Oh-My-Zsh and restoring ~/.zshrc (if present) ~~'
 
-   if [ ! -n "$(ls -A /home/vagrant/.oh-my-zsh)" ] 
+   if [[ ! -a /home/vagrant/.oh-my-zsh ]]
    	then
       echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-         echo "Attempting to Install Oh-My-Zsh..."
-
-         su vagrant sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-   	else
+         echo "Installing Oh-My-Zsh..."
+         su vagrant sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" 1> /dev/null 2>&1
         echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
         echo "Oh-My-Zsh is already installed"
-
    	fi
-
 
         echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 if [[ -a /home/vagrant/share/.zshrc ]] && [[ -a /home/vagrant/.zshrc ]]  
@@ -74,24 +72,22 @@ else
          echo "No .zshrc file was present."
 
 fi
-   echo -e ' \n '
+
 
       
-         if [ -n "$(ls -A /usr/bin/nodejs)" ]
+         if [[ -a /usr/bin/nodejs ]]
       then
           echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
           echo "Node is already installed"
 
 else
           echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-          echo "Attempting to Install Node..."
+          echo "Installing Node..."
 
-          curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-          sudo apt-get install -y nodejs
-          sudo apt-get install -y build-essential
+          curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - 1> /dev/null 2>&1
+          sudo apt-get install -y nodejs 1> /dev/null 2>&1
+          sudo apt-get install -y build-essential 1> /dev/null 2>&1
 fi
-   echo -e ' \n '
-
          if [[ ! -a /home/vagrant/node_modules ]]
       then
                   mkdir /home/vagrant/node_modules
@@ -104,7 +100,18 @@ else
           echo "Node modules directory already present"
 
 fi
-          echo -e ' \n '
+
+    #Install Heroku toolbelt
+ # config.vm.provision :shell, inline: "wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh"
+   if [[ ! -a /usr/bin/heroku ]]
+    then
+      echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+         echo "Installing Heroku Toolbelt..."
+         su vagrant sh -c "$(wget -O- https://toolbelt.heroku.com/install-ubuntu.sh)" 1> /dev/null 2>&1
+    else
+        echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+        echo "Heroku Toolbelt is already installed"
+    fi
 
    case $SHELL in
 */zsh)
